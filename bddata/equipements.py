bee62d3d444b042ee6257045dea83d13234b5cbc
leamiutnant <leamelcher@gmail.com>
2017-06-09 14:05:05 +0200

    Create equipements.py

--- bddata/equipements.py
+++ bddata/equipements.py
@@ -1,9 +1,13 @@
 import sqlite3
-"""
-classe Equipement avec les attributs, par rapport à ceux qu'on a choisit dans le csv_parser.
-"""
+
 class Equipement:
+    """
+    classe Equipement avec les attributs, par rapport à ceux qu'on a choisit dans le csv_parser.
+    """
     def __init__(self, j, numInstal, nomEq, city):
+        """
+        Méthode qui initialise l'objet Equipement avec ses attributs
+        """
         self.j = j
         self.numInstal = numInstal
         self.nomEq = nomEq
@@ -11,6 +15,9 @@ class Equipement:
 
 
     def insert(self, c):
+        """
+        Méthode qui insère les données dans la table correspondante
+        """
         #on insère les données dans la table equipements
         insert_query = "INSERT INTO equipements(j,numInstal, nomEq, city) VALUES (?,?,?,?)"
         c.execute(insert_query, (self.j, self.numInstal, self.nomEq, self.city))
