c526c5adf3b07702fcaf0cf6fa2c5dc9e2d32fff
leamiutnant <leamelcher@gmail.com>
2017-06-09 14:03:45 +0200

    Create activities.py

--- bddata/activities.py
+++ bddata/activities.py
@@ -4,6 +4,9 @@ import sqlite3
 classe Activity avec les attributs, par rapport à ceux qu'on a choisit dans le csv_parser.
 """
 class Activity:
+    """
+        Méthode qui initialise l'objet Activity avec ses attributs
+    """
     def __init__(self, j, equipId, activity, city, level):
         self.j = j
         self.equipId = equipId
@@ -13,6 +16,9 @@ class Activity:
 
 
     def insert(self, c):
+        """
+        Méthode qui insère les données dans la table correspondante
+        """
         #on insère les données dans la table activities
         insert_query = "INSERT INTO activities(j, equipId, activity, city, level) VALUES (?,?,?,?,?)"
         c.execute(insert_query, (self.j, self.equipId, self.activity, self.city, self.level))
