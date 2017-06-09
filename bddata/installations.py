d3bdcdcdb75c1f14318c837a858d873538e9a1cd
leamiutnant <leamelcher@gmail.com>
2017-06-09 14:05:43 +0200

    Create installations.py

--- bddata/installations.py
+++ bddata/installations.py
@@ -1,10 +1,13 @@
 import sqlite3
 
-"""
-classe Installation avec les attributs, par rapport à ceux qu'on a choisit dans le csv_parser.
-"""
 class Installation:
+    """
+    classe Installation avec les attributs, par rapport à ceux qu'on a choisit dans le csv_parser.
+    """
     def __init__(self, j, num, name, city, access):
+        """
+        Méthode qui initialise l'objet Installation avec ses attributs
+        """
         self.j = j
         self.num = num
         self.name = name
@@ -13,6 +16,9 @@ class Installation:
 
 
     def insert(self, c):
+        """
+        Méthode qui insère les données dans la table correspondante
+        """
         #on insère les données dans la table installations
         insert_query = "INSERT INTO installations(j, num, name, city, access) VALUES (?,?,?,?,?)"
         c.execute(insert_query, (self.j, self.num, self.name, self.city, self.access))
