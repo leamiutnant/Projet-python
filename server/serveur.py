6b520bf8d2178f4492a1935b16e09e7c590c95ca
leamiutnant <leamelcher@gmail.com>
2017-06-09 17:54:15 +0200

    Update serveur.py

--- server/serveur.py
+++ server/serveur.py
@@ -8,62 +8,69 @@ en utilisant la librairie bottle.
 """
 
 @route('/home')
+def home():
     """
     Lancement de la page où l'on doit sélectionner une city et/ou une activité,
     la page d'acceuil, en lui envoyant les données des villes et des activites.
     """
-    def home():
-        conn = sqlite3.connect("../bddata/database.db")
-        c = conn.cursor()
-        city = c.execute("SELECT a.city from activities a;")
-        l_city = [a for a in city]
-        c.close()
+    conn = sqlite3.connect("../bddata/database.db")
+    c = conn.cursor()
+    city = c.execute("SELECT a.city from activities a;")
+    l_city = [a for a in city]
 
-        conn = sqlite3.connect("../bddata/database.db")
-        c = conn.cursor()
-        activity = c.execute("SELECT a.activity from activities a;")
-        l_activity = [a for a in activity]
-        c.close()
-        return template('tpl/tmpt_search', city = l_city, activity = l_activity)
+    cities = []
+    for a in l_city:
+        cities.append(a[0])
+    cities = sorted(set(cities))
 
+
+    activity = c.execute("SELECT a.activity from activities a;")
+    l_activity = [a for a in activity]
+    c.close()
+
+    activities = []
+    for a in l_activity:
+        activities.append(a[0])
+    activities = sorted(set(activities))
+    return template('tpl/tmpt_search', city = cities, activity = activities)
 
 @post('/search')
+def search():
     """
     Méthode qui fait les requêtes sql pour afficher les résultats en fonction de la recherche effectuée.
     Elle récupère ce qui a été sélectionner sur le template tmpt_search.
     """
-    def search():
-        try:
-            #on se connecte à la bd.
-            conn = sqlite3.connect("../bddata/database.db")
-            c = conn.cursor()
-            #on récupère la ville et l'activité qui ont été sélectionné.
-            city = request.forms.get('city')
-            activity = request.forms.get('activity')
-            #Si l'utilisateur a sélectionné seulement une city on affiche toutes les activités de cette city
-            if(city != "all" and activity == "all"):
-                req = c.execute("SELECT a.activity, i.name, e.nomEq, a.city from installations i, equipements e, activities a where a.city=e.city and e.numInstal=i.num and a.city=\""+city+"\";")
-                list = [a for a in req]
-                #on envoie ces données au template tmpt_correct.
-                return template('tpl/tmpt_results', results=list)
-            #Si l'utilisateur a sélectionné une city et une activité, on affiche toutes les activités correspondant à celle sélectionnée dans la city.
-            if(city != "all" and activity != "all"):
-                req = c.execute("SELECT a.activity, i.name, e.nomEq, a.city from installations i, equipements e, activities a where a.city=e.city and e.numInstal=i.num and a.city=\""+city+"\" and a.activity=\""+activity+"\";")
-                list = [a for a in req]
-                #on envoie ces données au template tmpt_correct.
-                return template('tpl/tmpt_results', results=list)
-            #Si l'utilisateur a sélectionné seulement une activité on affiche toutes les citys où l'on peut pratiquer cette activité
-            if(city == "all" and activity != "all"):
-                req = c.execute("SELECT a.city, i.name, e.nomEq from installations i, equipements e, activities a where a.city=e.city and e.numInstal=i.num and a.activity=\""+activity+"\";")
-                list = [a for a in req]
-                #on envoie ces données au template tmpt_correct.
-                return template('tpl/tmpt_results', results=list)
-
+    try:
+        #on se connecte à la bd.
+        conn = sqlite3.connect("../bddata/database.db")
+        c = conn.cursor()
+        #on récupère la ville et l'activité qui ont été sélectionné.
+        city = request.forms.get('city')
+        activity = request.forms.get('activity')
+        #Si l'utilisateur a sélectionné seulement une city on affiche toutes les activités de cette city
+        if(city != "all" and activity == "all"):
+            req = c.execute("SELECT a.activity, i.name, e.nomEq, a.city from installations i, equipements e, activities a where a.city=e.city and e.numInstal=i.num and a.city=\""+city+"\";")
+        #Si l'utilisateur a sélectionné une city et une activité, on affiche toutes les activités correspondant à celle sélectionnée dans la city.
+        if(city != "all" and activity != "all"):
+            req = c.execute("SELECT a.activity, i.name, e.nomEq, a.city from installations i, equipements e, activities a where a.city=e.city and e.numInstal=i.num and a.city=\""+city+"\" and a.activity=\""+activity+"\";")
+        #Si l'utilisateur a sélectionné seulement une activité on affiche toutes les citys où l'on peut pratiquer cette activité
+        if(city == "all" and activity != "all"):
+            req = c.execute("SELECT \""+activity+"\", i.name, e.nomEq, a.city from installations i, equipements e, activities a where a.city=e.city and e.numInstal=i.num and a.activity=\""+activity+"\" limit 20;")
+        if(city == "all" and activity == "all"):
+        	req = []
+        if req != []:
+            list = [a for a in req]
             c.close()
+            #on envoie ces données au template tmpt_correct.
+            return template('tpl/tmpt_results', results=list)
+        else:
+            return("Veuillez rentrer au moins une ville ou une activité")
 
-        except Exception as error:
-            print("Unexpected error: {0}".format(error))
-            return("erreur")
+        
+
+    except Exception as error:
+        print("Unexpected error: {0}".format(error))
+        return("erreur")
 
 
 #on lance le serveur sur localhost, sur le port choisi
